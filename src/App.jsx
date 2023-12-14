646274b4995f6c68a50c8b13b33e94d9fecfff3e
safvankottayil <117747500+safvankottayil@users.noreply.github.com>
2023-12-14 22:07:43 +0530

    ddddsdd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import SiderbarLayoutRouter from "./router/SiderbarLayoutRouter"
 import Router from "./router/Router"
 function App() {
-
-
   return (
     <>
      <BrowserRouter>
